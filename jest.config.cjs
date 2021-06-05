79f9b3ef010584585cada3daed5221cc101cde86
thislooksfun <tlf@thislooks.fun>
2021-06-04 22:08:48 -0500

    test: fix the unit tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "node",
   transform: { "^.+\\.ts$": "ts-jest" },
+  moduleNameMapper: { "(.*).js$": ["$1"] },
   moduleFileExtensions: ["js", "mjs", "ts"],
   transformIgnorePatterns: [],
   collectCoverage: true,
--- test/index.test.ts
+++ test/index.test.ts
@@ -9,10 +9,14 @@ describe("validateYaml()", () => {
   it("should correctly parse and validate yaml files", () => {
     fc.assert(
       fc.property(fc.integer(), i => {
-        expect(validateYaml(`key: ${i}`, { key: "int" })).toStrictEqual([]);
-        expect(validateYaml(`other: ${i}`, { key: "int" })).toStrictEqual([
-          { msg: "missing key", path: "key" },
-          { msg: "extra key", path: "other" },
+        expect(validateYaml(`key: ${i}`, { key: "int" }).errors).toStrictEqual(
+          []
+        );
+        expect(
+          validateYaml(`other: ${i}`, { key: "int" }).errors
+        ).toStrictEqual([
+          { msg: "missing key 'key'", path: "", type: "val-end" },
+          { msg: "extra key", path: "other", type: "key" },
         ]);
       })
     );
@@ -21,48 +25,59 @@ describe("validateYaml()", () => {
 
 describe("validateFile()", () => {
   it("should error if the file does not exist", () => {
-    expect(validateFile("/some/fake/path", "")).toStrictEqual([
-      { msg: "File does not exist", path: "" },
+    expect(validateFile("/some/fake/path", "").errors).toStrictEqual([
+      { msg: "File does not exist", path: "", type: "meta" },
     ]);
   });
 
   it("should error if the path is not a file", () => {
-    expect(validateFile("/")).toStrictEqual([{ msg: "Not a file", path: "" }]);
+    expect(validateFile("/").errors).toStrictEqual([
+      { msg: "Not a file", path: "", type: "meta" },
+    ]);
   });
 
   it("should use the given structure", () => {
     const fpath = path.join(fixtureDir, "config/cat.yaml");
 
     const struct = { name: "string", images: "string[]" };
-    expect(validateFile(fpath, struct)).toStrictEqual([
-      { msg: "'[object Object]' is not a string", path: "images[0]" },
+    expect(validateFile(fpath, struct).errors).toStrictEqual([
+      {
+        msg: "'[object Object]' is not a string",
+        path: "images[0]",
+        type: "val-start",
+      },
     ]);
   });
 
   it("should error if it cannot find a structure", () => {
     const fpath = path.join(fixtureDir, "configless/something.invalid.yaml");
 
-    expect(validateFile(fpath)).toStrictEqual([
-      { msg: "Unable to determine expected structure for file", path: "" },
+    expect(validateFile(fpath).errors).toStrictEqual([
+      {
+        msg: "Unable to determine expected structure for file",
+        path: "",
+        type: "meta",
+      },
     ]);
   });
 
   it("should validate based on the found .yamlstructure", () => {
     expect(
-      validateFile(path.join(fixtureDir, "config/cat.yaml"))
+      validateFile(path.join(fixtureDir, "config/cat.yaml")).errors
     ).toStrictEqual([]);
 
     expect(
-      validateFile(path.join(fixtureDir, "config/snake.invalid.yaml"))
+      validateFile(path.join(fixtureDir, "config/snake.invalid.yaml")).errors
     ).toStrictEqual([
-      { msg: "missing key", path: "images[0].url" },
-      { msg: "extra key", path: "images[0].uri" },
+      { msg: "missing key 'url'", path: "images[0]", type: "val-end" },
+      { msg: "extra key", path: "images[0].uri", type: "key" },
     ]);
   });
 
   it("should use the nearest .yamlstructure", () => {
     expect(
       validateFile(path.join(fixtureDir, "config/burrowing/meerkat.yaml"))
+        .errors
     ).toStrictEqual([]);
   });
 });
