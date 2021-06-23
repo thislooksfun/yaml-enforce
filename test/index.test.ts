f8893c10974a142b261e982a7c03574ea65be543
thislooksfun <tlf@thislooks.fun>
2021-06-23 17:08:20 -0500

    test: add tests for filePosForErr()

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,120 @@
+import type { ValidationError } from "validate-structure";
+import type { Location, Range, RangeMap } from "../src/yaml";
 import fc from "fast-check";
 import path from "path";
 
 const fixtureDir = path.resolve(__dirname, "./fixtures");
 
-import { validateYaml, validateFile } from "../src";
+import { filePosForErr, validateYaml, validateFile } from "../src";
+
+describe("filePosForErr()", () => {
+  const fcInt = () => fc.integer();
+  const fcLoc = () => fc.record<Location>({ line: fcInt(), col: fcInt() });
+  const fcRange = () => fc.record<Range>({ start: fcLoc(), end: fcLoc() });
+
+  describe("ranges", () => {
+    it("should default to the start of the value range", () => {
+      fc.assert(
+        fc.property(fcRange(), valueRange => {
+          const err: ValidationError = { msg: "", type: "", path: [] };
+          const map: RangeMap = { valueRange };
+          expect(filePosForErr(err, map)).toStrictEqual(valueRange.start);
+        })
+      );
+    });
+
+    it("should use the end of the range if specified", () => {
+      fc.assert(
+        fc.property(fcRange(), valueRange => {
+          const err: ValidationError = { msg: "", type: "val-end", path: [] };
+          const map: RangeMap = { valueRange };
+          expect(filePosForErr(err, map)).toStrictEqual(valueRange.end);
+        })
+      );
+    });
+
+    it("should use key range if specified and present", () => {
+      fc.assert(
+        fc.property(fcRange(), fcRange(), (valueRange, keyRange) => {
+          const err: ValidationError = { msg: "", type: "key", path: [] };
+          const map: RangeMap = { valueRange, keyRange };
+          expect(filePosForErr(err, map)).toStrictEqual(keyRange.start);
+        })
+      );
+    });
+
+    it("should fall back on the start of the value range if the key range is missing", () => {
+      fc.assert(
+        fc.property(fcRange(), valueRange => {
+          const err: ValidationError = { msg: "", type: "key", path: [] };
+          const map: RangeMap = { valueRange };
+          expect(filePosForErr(err, map)).toStrictEqual(valueRange.start);
+        })
+      );
+    });
+  });
+
+  describe("keypaths", () => {
+    it("should follow the keypath", () => {
+      fc.assert(
+        fc.property(fcRange(), fcRange(), (vr1, vr2) => {
+          const err: ValidationError = {
+            msg: "",
+            type: "key",
+            path: ["this", "is", "the", "path"],
+          };
+
+          let map: RangeMap = { valueRange: vr2 };
+          for (let i = err.path.length - 1; i >= 0; --i) {
+            map = { valueRange: vr1, contents: { [err.path[i]]: map } };
+          }
+
+          expect(filePosForErr(err, map)).toStrictEqual(vr2.start);
+        })
+      );
+    });
+
+    it("should use the closest range to the given keypath", () => {
+      fc.assert(
+        fc.property(fcRange(), fcRange(), (vr1, vr2) => {
+          const err: ValidationError = {
+            msg: "",
+            type: "key",
+            path: ["this", "is", "the", "path"],
+          };
+
+          let map: RangeMap = { valueRange: vr2 };
+          for (let i = err.path.length - 2; i >= 0; --i) {
+            map = { valueRange: vr1, contents: { [err.path[i]]: map } };
+          }
+
+          expect(filePosForErr(err, map)).toStrictEqual(vr2.start);
+        })
+      );
+
+      fc.assert(
+        fc.property(fcRange(), fcRange(), (vr1, vr2) => {
+          const err: ValidationError = {
+            msg: "",
+            type: "key",
+            path: ["this", "is", "the", "path"],
+          };
+
+          let map: RangeMap = { valueRange: vr1 };
+          for (let i = err.path.length - 1; i >= 0; --i) {
+            if (i === 2) {
+              map = { valueRange: vr2, contents: { wrong: map } };
+            } else {
+              map = { valueRange: vr1, contents: { [err.path[i]]: map } };
+            }
+          }
+
+          expect(filePosForErr(err, map)).toStrictEqual(vr2.start);
+        })
+      );
+    });
+  });
+});
 
 describe("validateYaml()", () => {
   it("should correctly parse and validate yaml files", () => {
